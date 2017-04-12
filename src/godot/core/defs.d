6948de740d962e3216771e01350b13b80c5e5ab2
sheepandshepherd <sheepandshepherd@hotmail.com>
2017-04-12 15:19:05 +0200

    Add math constants to core defs

--- src/godot/core/defs.d
+++ src/godot/core/defs.d
@@ -8,6 +8,10 @@ alias Error = godot_error;
 alias real_t = float;
 
 
+enum real_t CMP_EPSILON = 0.00001;
+enum real_t CMP_EPSILON2 = (CMP_EPSILON*CMP_EPSILON);
+
+enum real_t _PLANE_EQ_DOT_EPSILON = 0.999;
+enum real_t _PLANE_EQ_D_EPSILON = 0.0001;
 
 
- 
