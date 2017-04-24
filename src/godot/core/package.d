c02cc5eaf56e3c32d69c094865cbec4d06119bd0
sheepandshepherd <sheepandshepherd@hotmail.com>
2017-04-24 17:25:27 +0200

    Add godot.core public import package

---
+++ src/godot/core/package.d
@@ -0,0 +1,29 @@
+/++
+Public import for all core types
++/
+module godot.core;
+
+public
+{
+	import godot.core.array;
+	import godot.core.basis;
+	import godot.core.color;
+	import godot.core.defs;
+	//import godot.core.dictionary;
+	//import godot.core.image;
+	import godot.core.inputevent;
+	import godot.core.nodepath;
+	import godot.core.plane;
+	import godot.core.poolarrays;
+	import godot.core.quat;
+	import godot.core.rect2;
+	import godot.core.rect3;
+	import godot.core.rid;
+	import godot.core.string;
+	import godot.core.transform;
+	import godot.core.transform2d;
+	import godot.core.variant;
+	import godot.core.vector2;
+	import godot.core.vector3;
+}
+
