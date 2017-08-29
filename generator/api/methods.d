a75e5ad85501e5379e53d0ddb502d9146b2ea0e1
sheepandshepherd <sheepandshepherd@hotmail.com>
2017-08-29 20:48:47 +0200

    Move old (temporary) method generator into methods.d

--- generator/api/methods.d
+++ generator/api/methods.d
@@ -36,6 +36,7 @@ class GodotMethod
 	}
 	
 	@serializationIgnore:
+	GodotClass parent;
 	
 	bool same(in GodotMethod other) const
 	{
@@ -74,7 +75,105 @@ class GodotMethod
 		return ret;
 	}
 	
-	GodotClass parent;
+	string source() const
+	{
+		string ret;
+		
+		ret ~= "\t";
+		ret ~= return_type.escapeType~" ";
+		// none of the types (Classes/Core/Primitive) are pointers in D
+		// Classes are reference types; the others are passed by value.
+		ret ~= escapeD(name);
+		if(has_varargs) ret ~= "(Args...)"; // D varargs template
+		ret ~= "(";
+		
+		bool hasDefault = false;
+		
+		foreach(ai, const a; arguments)
+		{
+			ret ~= a.type.dCallParamPrefix~a.type.escapeType~" "~escapeD(a.name);
+			
+			if(a.has_default_value || hasDefault)
+			{
+				ret ~= " = "~ escapeDefault(a.type, a.default_value);
+				hasDefault = true;
+			}
+			
+			if(ai != arguments.length-1) ret ~= ", ";
+		}
+		
+		if(has_varargs)
+		{
+			if(arguments.length > 0) ret ~= ", ";
+			//ret ~= "in ref Array _GODOT_var_args = Array.empty_array";
+			ret ~= "Args _GODOT_var_args"; // using D varargs template
+		}
+		
+		ret ~= ")";
+		if(is_const) ret ~= " const";
+		else if(name == "callv" && parent.name == "Object") ret ~= " const"; /// HACK
+		ret ~= "\n\t{\n";
+		
+		// implementation
+		if(is_virtual || has_varargs)
+		{
+			ret ~= "\t\tArray _GODOT_args = Array.empty_array;\n";
+			foreach(const a; arguments)
+			{
+				ret ~= "\t\t_GODOT_args.append("~escapeD(a.name)~");\n";
+			}
+			
+			if(has_varargs)
+			{
+				ret ~= "\t\tforeach(vai, VA; Args)\n\t\t{\n";
+				// TODO: check that VA can convert to Variant
+				ret ~= "\t\t\t_GODOT_args.append(_GODOT_var_args[vai]);\n";
+				ret ~= "\t\t}\n";
+			}
+			
+			ret ~= "\t\tString _GODOT_method_name = String(\""~name~"\");\n";
+			
+			ret ~= "\t\t";
+			if(return_type != "void") ret ~= "return ";
+			ret ~= "this.callv(_GODOT_method_name, _GODOT_args)";
+			if(return_type != "void" && return_type != "Variant")
+				ret ~= ".as!("~return_type.escapeType~")";
+			ret ~= ";\n";
+		} // end varargs/virtual impl
+		else
+		{
+			ret ~= "\t\tstatic godot_method_bind* mb = null;\n";
+			ret ~= "\t\tif(mb is null) ";
+			ret ~= "mb = godot_method_bind_get_method(\"" ~ parent.internal_name ~ "\", \"" ~ name ~ "\");\n";
+			
+			if(return_type != "void")
+			{
+				ret ~= "\t\t" ~ return_type.escapeType ~ " _GODOT_ret = "~return_type.emptyDefault~";\n";
+			}
+			
+			// create the arg array
+			if(arguments.length)
+			{
+			    ret ~= "\t\tconst(void*)["~text(arguments.length)~"] _GODOT_args = [";
+			    foreach(a; arguments) ret ~= "cast(void*)(" ~ a.type.ptrCallArgPrefix ~ a.name.escapeD ~ "), ";
+			    ret ~= "];\n";
+			}
+			
+			ret ~= "\t\tgodot_method_bind_ptrcall(mb, cast(godot_object)(this)";
+			ret ~= (arguments.length)?", _GODOT_args.ptr":", null";
+			if(return_type != "void") ret ~= (", cast(void*)&_GODOT_ret");
+			ret ~= ");\n";
+			if(return_type != "void")
+			{
+				/// TODO: this probably needs a cast to return_type
+				ret ~= "\t\treturn _GODOT_ret;\n";
+			}
+		} // end normal method impl
+		
+		ret ~= "\t}\n";
+		
+		return ret;
+	}
 }
 
 struct GodotArgument
