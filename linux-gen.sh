0c4195ca2c3a45592fdbdb644f341895fc309e8f
Supatier <atisuperati@gmail.com>
2017-12-21 10:18:42 +0700

    Update gitignore & add linux build

---
+++ linux-gen.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+#Godot path
+GODOT="godot"
+
+echo "==== Getting api.json ====="
+$GODOT --gdnative-generate-json-api api.json
+
+echo "==== Downloading gdnative_api.json ===="
+curl -O https://raw.githubusercontent.com/godotengine/godot/master/modules/gdnative/gdnative_api.json
+
+echo "==== Generating ===="
+dub run godot-d:generator
