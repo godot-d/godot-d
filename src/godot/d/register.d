3048700513f5feb0cb72e7135a243e54dcf113ee
sheepandshepherd <sheepandshepherd@hotmail.com>
2019-04-27 18:43:37 +0200

    Fix renamed classes. Closes #47

--- src/godot/d/register.d
+++ src/godot/d/register.d
@@ -180,13 +180,11 @@ void register(T)(void* handle, GDNativeLibrary lib) if(is(T == class))
 	else // base class is another D script
 	{
 		alias Base = BaseClassesTuple!T[0];
-		static if(hasUDA!(Base, Rename)) enum immutable(char*) baseName = TemplateArgsOf!(
-			getUDAs!(Base, Rename)[0])[0];
+		static if(hasUDA!(Base, Rename)) enum immutable(char*) baseName = godotName!Base;
 		else enum immutable(char*) baseName = fullyQualifiedName!Base;
 	}
 	
-	static if(hasUDA!(T, Rename)) enum immutable(char*) name = TemplateArgsOf!(
-		getUDAs!(T, Rename)[0])[0];
+	static if(hasUDA!(T, Rename)) enum immutable(char*) name = godotName!T;
 	else enum immutable(char*) name = fullyQualifiedName!T;
 	
 	auto icf = godot_instance_create_func(&createFunc!T, null, null);
