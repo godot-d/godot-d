011c7bc36d712b059c73047b8f3d56cea5b0c486
sheepandshepherd <sheepandshepherd@hotmail.com>
2019-10-12 00:16:36 +0200

    Add new extension types

--- src/godot/c/net.d
+++ src/godot/c/net.d
@@ -2,6 +2,8 @@ module godot.c.net;
 
 import godot.c.core;
 
+import core.stdc.stdint;
+
 @nogc nothrow:
 extern(C):
 
@@ -70,6 +72,76 @@ struct godot_net_multiplayer_peer
 	void* next; /* For extension? Or maybe not... */
 }
 
+struct godot_net_webrtc_library
+{
+	godot_gdnative_api_version version_; /* version of our API */
+
+	/* Called when the library is unset as default interface via godot_net_set_webrtc_library */
+	void* function() unregistered;
+
+	/* Used by WebRTCPeerConnection create when GDNative is the default implementation. */
+	/* Takes a pointer to WebRTCPeerConnectionGDNative, should bind and return OK, failure if binding was unsuccessful. */
+	godot_error* function(godot_object *) create_peer_connection;
+
+	void* next; /* For extension */
+}
+
+/* WebRTCPeerConnection interface */
+struct godot_net_webrtc_peer_connection
+{
+	godot_gdnative_api_version version_; /* version of our API */
+
+	godot_object *data; /* User reference */
+
+	/* This is WebRTCPeerConnection */
+	godot_int function(const void *) get_connection_state;
+
+	godot_error function(void *, const godot_dictionary *) initialize;
+	godot_object* function(void *, const char *p_channel_name, const godot_dictionary *) create_data_channel;
+	godot_error function(void *) create_offer;
+	godot_error function(void *) create_answer; /* unused for now, should be done automatically on set_local_description */
+	godot_error function(void *, const char *, const char *) set_remote_description;
+	godot_error function(void *, const char *, const char *) set_local_description;
+	godot_error function(void *, const char *, int, const char *) add_ice_candidate;
+	godot_error function(void *) poll;
+	void function(void *) close;
+
+	void* next; /* For extension? */
+}
+
+/* WebRTCDataChannel interface */
+struct godot_net_webrtc_data_channel
+{
+	godot_gdnative_api_version version_; /* version of our API */
+
+	godot_object *data; /* User reference */
+
+	/* This is PacketPeer */
+	godot_error function(void *, const uint8_t **, int *) get_packet;
+	godot_error function(void *, const uint8_t *, int) put_packet;
+	godot_int function(const void *) get_available_packet_count;
+	godot_int function(const void *) get_max_packet_size;
+
+	/* This is WebRTCDataChannel */
+	void function(void *, godot_int) set_write_mode;
+	godot_int function(const void *) get_write_mode;
+	bool function(const void *) was_string_packet;
+
+	godot_int function(const void *) get_ready_state;
+	const char* function(const void *) get_label;
+	bool function(const void *) is_ordered;
+	int function(const void *) get_id;
+	int function(const void *) get_max_packet_life_time;
+	int function(const void *) get_max_retransmits;
+	const char* function(const void *) get_protocol;
+	bool function(const void *) is_negotiated;
+
+	godot_error function(void *) poll;
+	void function(void *) close;
+
+	void* next; /* For extension? */
+}
+
 struct godot_net_webrtc_peer
 {
 	godot_gdnative_api_version ver; /* version of our API */
@@ -96,3 +168,4 @@ struct godot_net_webrtc_peer
 
 	void* next; /* For extension? */
 }
+
