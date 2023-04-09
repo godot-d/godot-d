7c1f6eb0b76582b006ecb95cc9cf8863ca7c88c7
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2023-04-09 00:16:33 -0700

    Add short versions of godot error codes

--- src/godot/package.d
+++ src/godot/package.d
@@ -7,3 +7,9 @@ public import godot.core;
 public import godot.d;
 public import godot.c.api : GDNativeVersion;
 
+public static immutable {
+	import godot.c.core : godot_error;
+
+	int OK = godot_error.GODOT_OK;
+	int ERR_FILE_EOF = godot_error.GODOT_ERR_FILE_EOF;
+}
